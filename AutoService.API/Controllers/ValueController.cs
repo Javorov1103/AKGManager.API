b89c6dda117eb46c1ded16646284e5c7be2278a9
Javorov1103 <k.javorov@gmail.com>
2020-12-01 22:23:53 +0200

    Initial

---
+++ AutoService.API/Controllers/ValueController.cs
@@ -0,0 +1,23 @@
+﻿
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+namespace AutoService.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValueController : Controller
+    {
+        [HttpGet]
+        [Authorize]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            return new string[] { "value1", "value2", "value3", "value4", "value5" };
+        }
+
+    }
+}
